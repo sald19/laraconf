86d6f6c895b7e3cadf5519ef91c2495f5c30a3cc
sald19 <carl.sald19@gmail.com>
2024-01-29 14:12:18 -0600

    add speaker resource and add new field json qualifications

--- app/Models/Speaker.php
+++ app/Models/Speaker.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use Filament\Forms\Components\CheckboxList;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -11,15 +14,59 @@ class Speaker extends Model
     use HasFactory;
 
     protected $fillable = [
-        'name',
-        'email',
         'bio',
+        'email',
+        'name',
+        'qualifications',
         'twitter_handle',
     ];
 
     protected $casts = [
         'id' => 'integer',
+        'qualifications' => 'array',
     ];
+
+    public static function getForm()
+    {
+        return [
+            TextInput::make('name')
+                ->required()
+                ->maxLength(255),
+            TextInput::make('email')
+                ->email()
+                ->required()
+                ->maxLength(255),
+            Textarea::make('bio')
+                ->required()
+                ->maxLength(65535)
+                ->columnSpanFull(),
+            TextInput::make('twitter_handle')
+                ->required()
+                ->maxLength(255),
+            CheckboxList::make('qualifications')
+                ->columnSpanFull()
+                ->bulkToggleable()
+                ->searchable()
+                ->options([
+                    'business-leader' => 'Business Leader',
+                    'charisma' => 'Charismatic Speaker',
+                    'first-time' => 'First Time Speaker',
+                    ' hometown-hero'=> 'Hometown Hero',
+                    'humanitarian'=> 'Works in Humanitarian Field',
+                    'Laracasts-contributor'=> 'Laracasts Contributor',
+                    'twitter-influencer' => 'Large Twitter Following',
+                    'youtube-influencer' => 'Large YouTube Following',
+                    'open-source' => 'Open Source Creator / Maintainer',
+                    'unique-perspective' => 'Unique Perspective'
+                ])
+                ->descriptions([
+                    'business-leader' => 'Here is a nice long description',
+                    'charisma' => 'this is event more information about you should pick this one',
+                ])
+                ->columns(3),
+
+        ];
+    }
 
     public function conferences(): BelongsToMany
     {
