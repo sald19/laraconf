114a9fefc5cedaf0f9c00a96f7c4bd80c1848de7
sald19 <carl.sald19@gmail.com>
2024-01-29 16:01:29 -0600

    refactor form layout conference

--- app/Models/Conference.php
+++ app/Models/Conference.php
@@ -5,7 +5,9 @@ namespace App\Models;
 use App\Enums\Region;
 use Filament\Forms\Components\CheckboxList;
 use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Fieldset;
 use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
@@ -56,50 +58,59 @@ class Conference extends Model
     public static function getForm(): array
     {
         return [
-            TextInput::make('name')
-                ->label('Conference')
-                ->required()
-                ->maxLength(255),
-            MarkdownEditor::make('description')
-                ->required()
-                ->maxLength(255),
-            DateTimePicker::make('start_date')
-                ->native(false)
-                ->required(),
-            DateTimePicker::make('end_date')
-                ->native(false)
-                ->required(),
-            Toggle::make('is_published')
-                ->default(true),
-            Select::make('status')
-                ->options([
-                    'draft' => 'Draft',
-                    'published' => 'Published',
-                    'archived' => 'Archived',
-                ])
-                ->required(),
-            Select::make('region')
-                ->live()
-                ->enum(Region::class)
-                ->options(Region::class),
-            Select::make('venue_id')
-                ->searchable()
-                ->preload()
-                ->createOptionForm(Venue::getForm())
-                ->editOptionForm(Venue::getForm())
-                ->relationship('venue', 'name', modifyQueryUsing: function (Builder $query, Get $get) {
-                    return $query->where('region', $get('region'));
-                }),
-            CheckboxList::make('speakers')
-                ->relationship('speakers', 'name')
-                ->bulkToggleable()
-                ->searchable()
-                ->options(Speaker::pluck('name', 'id'))
-                ->descriptions([
-                    'business-leader' => 'Here is a nice long description',
-                    'charisma' => 'this is event more information about you should pick this one',
-                ])
-                ->required(),
+            Section::make('Conference Details')
+                ->collapsible()
+                ->description('Provide some basic information about the conference.')
+                ->icon('heroicon-o-information-circle')
+                ->columns(2)
+                ->schema([
+                    TextInput::make('name')
+                        ->columnSpanFull()
+                        ->label('Conference Name')
+                        ->default('My Conference')
+                        ->required()
+                        ->maxLength(60),
+                    MarkdownEditor::make('description')
+                        ->columnSpanFull()
+                        ->required()
+                        ->maxLength(255),
+                    DateTimePicker::make('start_date')
+                        ->native(false)
+                        ->required(),
+                    DateTimePicker::make('end_date')
+                        ->native(false)
+                        ->required(),
+                    Fieldset::make('Status')
+                            ->columns(1)
+                        ->schema([
+                            Select::make('status')
+                                ->options([
+                                    'draft' => 'Draft',
+                                    'published' => 'Published',
+                                    'archived' => 'Archived',
+                                ])
+                                ->required(),
+                            Toggle::make('is_published')
+                                ->default(true),
+
+                        ]),
+                ]),
+            Section::make('Location')
+                ->columns(2)
+                ->schema([
+                    Select::make('region')
+                        ->live()
+                        ->enum(Region::class)
+                        ->options(Region::class),
+                    Select::make('venue_id')
+                        ->searchable()
+                        ->preload()
+                        ->createOptionForm(Venue::getForm())
+                        ->editOptionForm(Venue::getForm())
+                        ->relationship('venue', 'name', modifyQueryUsing: function (Builder $query, Get $get) {
+                            return $query->where('region', $get('region'));
+                        }),
+                ]),
         ];
     }
 }
