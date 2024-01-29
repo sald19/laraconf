6ec51a5f6e2f0c1f9ce2ec43b243f63dc32c33d7
sald19 <carl.sald19@gmail.com>
2024-01-29 17:04:45 -0600

    upddate factory

--- database/factories/ConferenceFactory.php
+++ database/factories/ConferenceFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Enums\Region;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 use App\Models\Conference;
@@ -22,12 +23,16 @@ class ConferenceFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
-            'description' => $this->faker->text(),
+            'name' => $this->faker->name,
+            'description' => $this->faker->text,
             'start_date' => $this->faker->dateTime(),
             'end_date' => $this->faker->dateTime(),
-            'status' => $this->faker->word(),
-            'region' => $this->faker->word(),
+            'status' => $this->faker->randomElement([
+                'draft',
+                'published',
+                'archived',
+            ]),
+            'region' => $this->faker->randomElement(Region::class),
             'venue_id' => Venue::factory(),
         ];
     }
