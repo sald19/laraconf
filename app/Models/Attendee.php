c1f3673137ac02327f77b4595aa2c89ce6109be8
sald19 <carl.sald19@gmail.com>
2024-02-02 10:29:48 -0600

    implement livewire page sign up

--- app/Models/Attendee.php
+++ app/Models/Attendee.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -10,8 +12,31 @@ class Attendee extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'conference_id',
+        'email',
+        'is_paid',
+        'name',
+        'ticket_cost',
+    ];
+
     public function conference(): BelongsTo
     {
         return $this->belongsTo(Conference::class);
+    }
+
+    public static function getForm(): array
+    {
+        return [
+            Group::make()->columns(2)->schema([
+                TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->maxLength(255),
+            ]),
+        ];
     }
 }
